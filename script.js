38f65bb13eb9e1faf371781e0d0a0726a0652f59
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-01-29 20:07:35 +0000

    Add 16x16 grid to webpage

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, World!");
+const containerDiv = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    const gridDiv = document.createElement("div");
+    containerDiv.appendChild(gridDiv)
+    gridDiv.textContent = "Square";
+};
