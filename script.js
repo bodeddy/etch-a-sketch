0aa08ce531903f66df64b959bd7af6a3d7232387
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-01-31 19:38:00 +0000

    Add class to divs

--- script.js
+++ script.js
@@ -4,4 +4,5 @@ for (let i = 0; i < 16; i++) {
     const gridDiv = document.createElement("div");
     containerDiv.appendChild(gridDiv)
     gridDiv.textContent = "Square";
+    gridDiv.classList.add("grid");
 };
