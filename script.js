a991ca9862d78fbe81ca4d745b3a0e408612e064
bodeddy <107798427+bodeddy@users.noreply.github.com>
2026-02-17 19:51:15 +0000

    Ref container and create square

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+// Build a browser version of something between a sketchpad and an Etch-A-Sketch.
+
+// Reference a container for the grid and create a grid square
+
+const container = document.querySelector("#container");
+
+function createSquare () {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.append(square);
+}
+
+createSquare();
